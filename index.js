d386c8004847177a984d21a1e1c1805334d68e0e
John Keisker <jkeisker@me.com>
2016-03-14 14:22:37 -0500

    completed homework

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("test");
+//3
+var one;
+var two;
+var three;
+var four;
+var five;
+//4
+var numberOne = 1;
+var numberTwo = 2;
+var numberThree = 3;
+var numberFour = 4;
+var numberFive = 5;
+//5
+var something = "something";
+var food = "get in my belly";
+var fire = "hot";
+var ice = "cold";
+var broccoli = "gross";
+//6
+var monday = true;
+var tuesday = false;
+var wednesday = true;
+var thursday = false;
+var friday = true;
+//7
+var potatoes = 1;
+var gravy = 2;
+var sideDish = potatoes + gravy;
+//8
+var phish = true;
+if (phish) {
+  console.log ("play song");
+}
+//9
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+
+}
